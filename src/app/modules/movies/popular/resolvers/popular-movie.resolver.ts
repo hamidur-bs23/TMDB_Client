7adc3ebc61b8716ab2e845bcb76893c74059519e
Hamidur-BS23 <hamidur.rhaman@brainstation-23.com>
2023-01-16 18:57:51 +0600

    popular movies resolver (not stable)

--- src/app/modules/movies/popular/resolvers/popular-movie.resolver.ts
+++ src/app/modules/movies/popular/resolvers/popular-movie.resolver.ts
@@ -1,35 +1,34 @@
-import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
-import { PopularMovieResponse } from '../../models/movie.model';
-import { select, Store } from '@ngrx/store';
-import { PopularMovieState } from '../store/popular.state';
-import { selectMovies } from '../store/popular.selector';
-import { PopularStoreService } from '../store/popular.store.service';
-import { tap } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class PopularMovieResolver implements Resolve<PopularMovieResponse> {
-  constructor(
-    private store: Store<PopularMovieState>,
-    private popularStoreService: PopularStoreService
-  ) {}
-
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<PopularMovieResponse>
-    | Promise<PopularMovieResponse>
-    | PopularMovieResponse {
-    this.popularStoreService.getPopularMovieState$.pipe(
-      tap((state: PopularMovieState) => {
-        if (state.currentPageIndex > 0 && state.movies.length > 0) {
-        }
-      })
-    );
-
-    return undefined;
-  }
-}
+// import { Injectable } from '@angular/core';
+// import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+// import { PopularMovieResponse } from '../../models/movie.model';
+// import { Store } from '@ngrx/store';
+// import { PopularMovieState } from '../store/popular.state';
+// import { PopularStoreService } from '../store/popular.store.service';
+// import {Observable, tap} from 'rxjs';
+//
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class PopularMovieResolver implements Resolve<PopularMovieResponse> {
+//   constructor(
+//     private store: Store<PopularMovieState>,
+//     private popularStoreService: PopularStoreService
+//   ) {}
+//
+//   resolve(
+//     route: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot
+//   ):
+//     | Observable<PopularMovieResponse>
+//     | Promise<PopularMovieResponse>
+//     | PopularMovieResponse {
+//     this.popularStoreService.getAllPopularMovies$.pipe(
+//       tap((state: PopularMovieState) => {
+//         if (state.currentPageIndex > 0 && state.movies.length > 0) {
+//         }
+//       })
+//     );
+//
+//     return true;
+//   }
+// }
